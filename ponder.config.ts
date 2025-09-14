94949b5e10db765a15dc90cdb15f61be70982127
Jorge Pablo Franetovic Stocker <jpfraneto@gmail.com>
2025-09-14 08:25:33 -0300

    aloja

--- ponder.config.ts
+++ ponder.config.ts
@@ -16,7 +16,7 @@ export default createConfig({
       abi: MemeticSignalProtocolAbi,
       address: "0x241dc35F698d3bA887Fffc30A6d6bf0E05FF46D8",
       chain: "base",
-      startBlock: 35501865, //35279188, // Deployment block: https://basescan.org/address/0x241dc35F698d3bA887Fffc30A6d6bf0E05FF46D8#code
+      startBlock: 35279188, //35279188, // Deployment block: https://basescan.org/address/0x241dc35F698d3bA887Fffc30A6d6bf0E05FF46D8#code
     },
   },
 });
